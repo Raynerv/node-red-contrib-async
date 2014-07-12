0cf2d23e38e77bad0f4afe50c4c999ef485928d1
Rayner Vintervoll <raynerv@gmail.com>
2014-07-12 18:44:22 +0200

    Added Async library to sandbox context

--- async.js
+++ async.js
@@ -14,66 +14,69 @@
  * limitations under the License.
  **/
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     var util = require("util");
     var vm = require("vm");
     var fs = require('fs');
     var fspath = require('path');
-	var _ = require('underscore');
-	
-	var handleResults = function(ctx, msg, results) {		
-		if (results == null) {
-			results = [];
-		} else if (results.length == null) {
-			results = [results];
-		}
-		if (msg._topic) {
-			for (var m in results) {
-				if (results[m]) {
-					if (util.isArray(results[m])) {
-						for (var n in results[m]) {
-							results[m][n]._topic = msg._topic;
-						}
-					} else {
-						results[m]._topic = msg._topic;
-					}
-				}
-			}
-		}
-		ctx.send(results);
-	};    
+    var _ = require('underscore');
+    var async = require('async');
+
+    var handleResults = function (ctx, msg, results) {
+        if (results == null) {
+            results = [];
+        } else if (results.length == null) {
+            results = [results];
+        }
+        if (msg._topic) {
+            for (var m in results) {
+                if (results[m]) {
+                    if (util.isArray(results[m])) {
+                        for (var n in results[m]) {
+                            results[m][n]._topic = msg._topic;
+                        }
+                    } else {
+                        results[m]._topic = msg._topic;
+                    }
+                }
+            }
+        }
+        ctx.send(results);
+    };
 
     function AsyncFunctionNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.name = n.name;
         this.async = n.async;
-        var functionText = "var results = (function(msg){"+this.async+"\n})(msg);";
-				
+        var functionText = "var results = (function(msg){" + this.async + "\n})(msg);";
+
         this.topic = n.topic;
-        this.context = {global:RED.settings.functionGlobalContext || {}};
-						
+        this.context = {global: RED.settings.functionGlobalContext || {}};
+
         try {
             this.script = vm.createScript(functionText);
-            this.on("input", function(msg) {
+            this.on("input", function (msg) {
                 if (msg != null) {
-					var ctx = this;
-					var cb = function(result) { handleResults(ctx, msg, result) };
-                    var sandbox = {cb:cb, msg:msg,console:console,util:util,Buffer:Buffer,context:this.context, _:_} ;
+                    var ctx = this;
+                    var cb = function (result) {
+                        handleResults(ctx, msg, result)
+                    };
+                    var sandbox = {cb: cb, msg: msg, console: console, util: util, Buffer: Buffer, context:
+                        this.context, _: _, async: async};
                     try {
                         this.script.runInNewContext(sandbox);
                         var results = sandbox.results;
-						handleResults(this, msg, results);						                    
-
-                    } catch(err) {
+                        handleResults(this, msg, results);
+                    } catch (err) {
                         this.error(err.toString());
                     }
                 }
             });
-        } catch(err) {
+        } catch (err) {
             this.error(err);
         }
     }
 
-    RED.nodes.registerType("async",AsyncFunctionNode);
+    RED.nodes.registerType("async", AsyncFunctionNode);
     RED.library.register("async");
 }
